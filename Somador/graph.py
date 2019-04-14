f5cdeeb930ba0cd3a5cf5d428b13219419bbe029
Amanda Oliveira <amandacnoliveira@poli.ufrj.br>
2019-04-13 23:54:08 -0300

    Somador fully implemented. Added graph code to make efficiency plot.

---
+++ Somador/graph.py
@@ -0,0 +1,84 @@
+#!/usr/bin/python
+
+
+import argparse
+import math
+import matplotlib.pyplot as plt
+# from matplotlib import rc
+
+# rc('text', usetex=True)
+
+parser = argparse.ArgumentParser(description='Create efficiency graphs')
+
+required = parser.add_argument_group('required arguments')
+required.add_argument('-N', metavar='NUBER-OF-VALUES', type=int, required=True,
+                      help='Number of values that were summed together by the code.')
+required.add_argument('-i', '--inputFileName', type=str, required=True,
+                      help='Name of the input file. Should be formed by the main file.')
+
+parser.add_argument('-o', '--outputFileRoot', type=str,
+                    default="latencyGraph",
+                    help='Root of the name of the output graph.')
+
+args = parser.parse_args()
+
+
+# Main function
+if __name__ == "__main__":
+    N = args.N
+
+    # Variables to be filled
+    nThrVec = []
+    timesDict = {}
+
+    # Reading from file
+    infile = open(args.inputFileName, "r")
+
+    lines = infile.readlines()
+    for line in lines:
+        words = line.split(' ')
+
+        try:
+            thisN = int(words[0])
+        except:
+            continue
+
+        if thisN == N:  # Same N as desired
+            K = int(words[1])
+            t = float(words[2])
+
+            # Add the K value to the list, if it has not
+            # been done yet, and t to dictionary
+            if K not in nThrVec:
+                nThrVec += [K]
+                timesDict.update( { K: [t] } )
+            else:
+                timesDict[K] += [t]
+
+    nThrVec.sort()
+
+    # Create lists of execution time and uncertainty per K
+    timeVec = []
+    errTVec = []
+    for i in range(len(nThrVec)):
+        listOfTimes = timesDict[nThrVec[i]]
+        timeVec += [ sum(listOfTimes)/len(listOfTimes) ]
+
+        tmp = [ (x - timeVec[i])**2 for x in listOfTimes]
+        errTVec += [ math.sqrt( sum( tmp )/len(listOfTimes) ) ]
+
+    fig, ax = plt.subplots(figsize=(10, 6))
+    fig.suptitle("Latency of the sum algorithm",
+                 y=0.96, fontweight='bold', fontsize=17)
+    # fig.suptitle(r"Lat\{^}encia do somador",
+    #              fontweight='bold', fontsize=15)
+    plt.errorbar(nThrVec, timeVec, errTVec, ecolor='black', elinewidth=.5,
+                 marker='.', mec='black', mfc='black', ms=10)
+    plt.xlabel('Number of threads')
+    ax.set_xscale('log', basex=2)
+    plt.ylabel('Execution time (s)')
+    # plt.xlabel(r'N\'{u}mero de threads')
+    # plt.ylabel(r'Tempo de execu\c{c}\~{a}o (s)')
+    plt.subplots_adjust(left=0.1, right=0.94, top=0.89)
+    outputFileName = "%s_%ielements.pdf"%(args.outputFileRoot, N)
+    fig.savefig(outputFileName)
--- Somador/main.cpp
+++ Somador/main.cpp
@@ -1,6 +1,8 @@
 #include <stdint.h>
 #include <stdlib.h>
 #include <iostream>
+#include <fstream>
+#include <sstream>
 #include <time.h>
 #include <atomic>
 #include <vector>
@@ -14,12 +16,12 @@ using namespace std;
 //    bool held;
 //};
 
-void acquire(std::atomic_flag lock){
-    while (lock.test_and_set()) ;
+void acquire(std::atomic_flag* lock){
+    while (lock->test_and_set()) ;
 }
 
-void release(std::atomic_flag lock){
-    lock.clear();
+void release(std::atomic_flag* lock){
+    lock->clear();
 }
 
 
@@ -27,7 +29,6 @@ void release(std::atomic_flag lock){
 // Global variables:
 int64_t totalSum = 0;
 std::atomic_flag sumLock = ATOMIC_FLAG_INIT;
-//Lock sumLock;
 // ------------------
 
 
@@ -39,19 +40,41 @@ void vectorSum(std::vector<int8_t> v){
     for(std::vector<int8_t>::iterator it = v.begin(); it != v.end(); ++it)
         sumOfValues += *it;
 
-    // cout << "Got sum " << sumOfValues << endl;
+    // Write value to the memory -- CRITICAL REGION
+    acquire(&sumLock);
+    totalSum = totalSum + sumOfValues;
+    release(&sumLock);
+    // End of critial region
 }
 
 
 
 int main(int argc, char* argv[]){
-    // TODO: What arguments should be given to the code?
-    // Could choose to select the N and K, or test all values of K and N...
-    int     K=1;
-    int64_t N=1e9;
+    if (argc < 3){
+        cout << "ERROR: Not enough arguments." << endl;
+        cout << "       This program needs to be given the number of values to "
+             << "be summed and the number of threads to do so." << endl;
+        cout << "       Also can be given the name of output file. Default set "
+             << "to 'sumLatencies'" << endl;
+        cout << "       >>> ./main N_VALUES N_THREADS [OUTPUT_NAME]" << endl;
+        exit(1);
+    }
+
+    int64_t N;
+    int K = atoi(argv[2]);
+    string fileRoot;
+
+    std::istringstream iss(argv[1]);
+    iss >> N;
+
+    if (argc > 3) fileRoot = argv[3];
+    else fileRoot = "sumLatencies";
+
+    string filename = fileRoot + ".txt";
 
     cout << N << " values to sum. " << K << " threads." << endl;
 
+    sumLock.clear();
 
     // Creation and filling of the vector of numbers
     std::vector<int8_t> list;
@@ -63,6 +86,7 @@ int main(int argc, char* argv[]){
     }
 
     // Execution of the sum itself
+    cout << "Will start to calculate the sum now" << endl;
     clock_t t_0 = clock();
 
     std::vector<std::thread> threadsVec;
@@ -72,10 +96,9 @@ int main(int argc, char* argv[]){
     vector<int8_t>::iterator last = begin + numbPerThr;
 
     for (int thrIdx; thrIdx<K; thrIdx++){
-        cout << "Thread " << thrIdx + 1 << ", begining in " <<
-            begin - list.begin() + 1 << " and finishing in "
-            << last - list.begin() << endl;
-
+        // cout << "Thread " << thrIdx + 1 << ", begining in " <<
+        //     begin - list.begin() + 1 << " and finishing in "
+        //     << last - list.begin() << endl;
         threadsVec.push_back( std::thread( vectorSum, std::vector<int8_t>(begin, last) ) );
 
         begin = last;
@@ -89,5 +112,16 @@ int main(int argc, char* argv[]){
         threadsVec[thrIdx].join();
     }
     clock_t t = clock();
-    cout << "Finished all threads. Time to calculate sum: " << (float)t/CLOCKS_PER_SEC << "s." << endl;
+    double totTime = (float)(t - t_0)/CLOCKS_PER_SEC;
+    cout << "Finished all threads. Total sum is " << totalSum << " Time to calculate sum: " << totTime << "s." << endl;
+
+    // Will write data to file
+    cout << "Writing to file '" << filename << "'" << endl;
+    std::ofstream outfile;
+    outfile.open(filename, std::ios_base::app);
+    outfile << N << " " << K << " " << totTime << " " << endl;
+    outfile.close();
+    cout << "Done. Finishing program." << endl;
+
+    return 0;
 }
