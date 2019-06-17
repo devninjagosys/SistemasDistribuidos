164a830b6eb5b3dfe3c1bfc88b0e392f1fba955f
devninjagosys <45133384+devninjagosys@users.noreply.github.com>
2019-06-17 01:37:26 -0300

    Update Valentao.h

--- Valentao/Valentao.h
+++ Valentao/Valentao.h
@@ -1,3 +1,4 @@
+
 /*
  *  Header for the 'valentao' code
  *  Author: Amanda, Vinicius
@@ -6,6 +7,22 @@
 
 #include <vector>
 
+
+//LOCKS
+std::mutex electionVectorMutex; 
+std::mutex mtx_isCheckingOnLeader; 
+std::mutex mtx_isOperational;
+std::mutex mtx_leaderIdx;
+std::mutex mtx_isSilenced;
+std::mutex mtx_outElection;
+std::mutex mtx_leaderAnswered;
+std::mutex mtx_msgCount;
+std::mutex mtx_outMsgCount;
+std::mutex mtx_inMsgCount;
+std::mutex mtx_electionNumber;
+std::mutex mtx_totalElectionMsgs;
+std::mutex mtx_alreadyOnThreadleader;
+std::mutex mtx_vivo_ok;
 
 // Processes class
 class ProcessClient {
@@ -29,27 +46,36 @@ int server_socket;         // This process' socket
 int selfID;
 int leaderIdx;   // leaderIdx should be the position in the process vector, or -1 if the leader is the process itslef
 int selfElecValue;
-
 int messageLength;
+int electionNumber=0;
+int totalElectionMsgs=0;
+int caseNumber=0;
+int number_vivo_ok=0;
 
 char* delimiter;
 
 int msgCount[5] = {0, 0, 0, 0, 0};
 int outMsgCount = 0;
+int inMsgCount = 0;
+
 
 std::vector<int> ongoingElections;
 
 std::vector<ProcessClient> processes;
-#define N_PROC 5      // Number of processes operating
+int N_PROC;     // Number of processes operating
 
-std::chrono::high_resolution_clock::time_point start_time;
-std::chrono::high_resolution_clock::time_point actual_time;
+// std::chrono::high_resolution_clock::time_point start_time;
+// std::chrono::high_resolution_clock::time_point actual_time;
 
 
 // Global flags
+bool isOperational;
 bool leaderAnswered;
 bool isSilenced;
-bool resetTimer=false;
+bool isCheckingOnLeader;
+bool alreadyOnThreadleader=false;
+bool outElection=false;
+// bool resetTimer=false;
 
 
 // Message types
@@ -60,14 +86,41 @@ enum MessageTyp {
     m_vivo = 3,
     m_vivo_ok = 4
 };
+//Lock function;
+int get_electionSize();
+int erase_ongoingElections();
+bool get_isOperational();
+void set_isOperational(bool temp);
+bool get_isCheckingOnLeader();
+void set_isCheckingOnLeader(bool temp);
+int get_leaderIdx();
+void set_leaderIdx(int temp);
+bool get_isSilenced();
+void set_isSilenced(bool temp);
+int get_inMsgCount();
+void increment_inMsgCount();
+int get_outMsgCount();
+void increment_outMsgCount();
+bool get_leaderAnswered();
+void set_leaderAnswered(bool temp);
+bool get_outElection();
+void set_outElection(bool temp);
+void increasing_msgCount(int temp);
+int get_electionNumber();
+void set_electionNumber();
+int get_totalElectionMsgs();
+void set_totalElectionMsgs(int temp);
+int get_alreadyOnThreadleader();
+void set_alreadyOnThreadleader(int temp);
+
 
 // Main funtions
 int main(int argc, char* argv[]);
 
-void interface();
-void *communication(void*);
-void *leader(void*);
-
-
 // Auxiliary funtions
 int setupServerSocket(int port);
+void* electionFinish(void*);
+void *caseResolver(void*);
+void* interface(void*);
+void* communication(void*);
+void* leader(void*);
